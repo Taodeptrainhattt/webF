099d3d4b52079ecb42ea73f5c1985a448550f4fe
Taodeptrainhattt <hazajester@gmail.com>
2026-02-09 20:45:16 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import user from "../models/User.js";
 
 const router = express.Router();
 
@@ -42,3 +42,4 @@ router.post("/login", async (req, res) => {
 });
 
 export default router;
+
